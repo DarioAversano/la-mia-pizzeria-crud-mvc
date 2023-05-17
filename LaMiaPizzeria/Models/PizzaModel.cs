c2924837e16e091b6bd8ddbb45c4885ce5decce6
darioaversano <risusveloce@gmail.com>
2023-05-17 14:55:38 +0200

    commit pizza model 2

--- LaMiaPizzeria/Models/PizzaModel.cs
+++ LaMiaPizzeria/Models/PizzaModel.cs
@@ -1,6 +1,31 @@
-﻿namespace LaMiaPizzeria.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace LaMiaPizzeria.Models
 {
     public class PizzaModel
     {
+        public int Id { get; set; }
+        [MaxLength(40)]
+        public string NomePizza { get; set; }
+        [Column(TypeName = "text")]
+        public string Descrizione { get; set; }
+        [MaxLength(300)]
+        public string Immagine { get; set; }
+        [MaxLength(100)]
+        public float Prezzo { get; set; }
+
+    
+
+    public PizzaModel(int id, string nomePizza, string descrizione, string immagine, float prezzo)
+    {
+        Id = id;
+        NomePizza = nomePizza;
+        Descrizione = descrizione;
+        Immagine = immagine;
+            Prezzo = prezzo;
     }
+
+}
 }
