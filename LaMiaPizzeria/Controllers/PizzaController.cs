d390587d4797888ec2d14d850cc6054de834c4c7
risus <risus@DESKTOP-HQ58SHQ>
2023-05-19 16:58:30 +0200

    Return Views Modifica Pizza

--- LaMiaPizzeria/Controllers/PizzaController.cs
+++ LaMiaPizzeria/Controllers/PizzaController.cs
@@ -1,8 +1,6 @@
 ﻿using LaMiaPizzeria.DataBase;
 using LaMiaPizzeria.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using LaMiaPizzeria.Controllers;
 
 namespace LaMiaPizzeria.Controllers
 {
@@ -15,9 +13,9 @@ namespace LaMiaPizzeria.Controllers
                 List<PizzaModel> pizze = db.Pizza.ToList();
                 return View("Index", pizze);
             }
-            
-        }
 
+        }
+        // AZIONE PER LA CREAZIONE DI UNA PIZZA SIMILE A QUELLA DELLA MODIFICA 
         [HttpGet]
         public IActionResult Creazione()
         {
@@ -42,8 +40,101 @@ namespace LaMiaPizzeria.Controllers
             }
 
         }
+        // azione COME CRAZIONE, SIMILE
+        [HttpGet]
+        public IActionResult modificaPizza(int id)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                PizzaModel? PizzaModifica = db.Pizza.Where(Pizza => Pizza.Id == id).FirstOrDefault();
+                return View("modificaPizza", PizzaModifica);
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult modificaPizza(int id, PizzaModel pizzaModificata)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("modificaPizza", pizzaModificata);
+            }
+
+            using (PizzaContext db = new PizzaContext())
+            {
+                PizzaModel? pizzaDaModificare = db.Pizza.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if (pizzaDaModificare != null)
+                {
+                    pizzaDaModificare.NomePizza = pizzaModificata.NomePizza;
+                    pizzaDaModificare.Descrizione = pizzaModificata.Descrizione;
+                    pizzaDaModificare.Immagine = pizzaModificata.Immagine;
+                    pizzaDaModificare.Prezzo = pizzaModificata.Prezzo;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound("La pizza non è stata trovata");
+                }
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreaNuovaPizza(PizzaModel nuovaPizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Creazione", nuovaPizza);
+            }
+
+            using (PizzaContext db = new PizzaContext())
+            {
+                db.Pizza.Add(nuovaPizza);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+        }
+
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult eliminaPizza(int id)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                PizzaModel? pizzaDaEliminare = db.Pizza.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if (pizzaDaEliminare != null)
+                {
+                    db.Remove(pizzaDaEliminare);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound("La pizza non è stata trovata");
+                }
+
+            }
+        }
+
+
+
+
 
 
 
     }
+
 }
+
+
